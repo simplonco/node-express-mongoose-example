b12bed8fdef71b37abe60f4cc7470504daf2f58f
Sandro Micalizzi <hello@sandromicalizzi.com>
2017-08-24 11:50:25 +0200

    add node_modules in .gitignore + add model learner + install mongoose

--- server.js
+++ server.js
@@ -1,10 +1,15 @@
 const path = require('path');
 const express = require('express');
 const logger = require('morgan');
+const mongoose = require('mongoose');
 
 const routes = require('./routes');
 
 const app = express();
+
+mongoose.connect('mongodb://localhost/promo8', () => {
+  console.log('MongoDB connection success');
+});
 
 app.set('view engine', 'pug');
 app.set('views', path.resolve(__dirname, 'views'));
@@ -14,6 +19,6 @@ app.use(express.static(path.resolve(__dirname, 'public')));
 app.use(logger('dev'));
 app.use(routes);
 
-app.listen(3000, () => {
-  console.log('Server is running on port 3000');
+app.listen(3001, () => {
+  console.log('Server is running on port 3001');
 })
